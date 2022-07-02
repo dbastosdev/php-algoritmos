d0d11cbbf1b1e321847ea54f9469ea917b41fc6a
dbastosdev <dbastos.dev@gmail.com>
2022-07-01 21:34:01 -0300

    final solution

--- binary-search.php
+++ binary-search.php
@@ -6,6 +6,8 @@ algorithm of binary search
 
 $numbers = [1,2,3,4,5,6,7,8,9,10];
 
+
+// First solution
 function binarySearch($number, $array) {
     $init = $array[0];
     $finish = $array[count($array) - 1];
@@ -26,9 +28,33 @@ function binarySearch($number, $array) {
     return false;
 }
 
+// Final soluction
+function binarySearch2($number, $array) {
+    $init = 0;
+    $finish = count($array) - 1;
+    $mid = ceil(($init + $finish) / 2);
+    $n = $array[$mid];
+
+    while($init < $finish) {
+        $n = $array[$mid];
+        if($number == $array[$mid]){
+            return $mid;
+        } else if($array[$mid] > $number){
+            $finish = $mid;
+            $mid = ceil(($init + $finish) / 2);
+        } else {
+            $init = $mid;
+            $mid = ceil(($init + $finish) / 2);
+        }
+    }
+
+    return -1;
+}
+
+
 // Results and applications
-var_dump(binarySearch(7,$numbers));
-var_dump(binarySearch(5,$numbers));
-var_dump(binarySearch(50,$numbers));
+var_dump(binarySearch2(7,$numbers));
+var_dump(binarySearch2(5,$numbers));
+var_dump(binarySearch2(11,$numbers));
 
 
